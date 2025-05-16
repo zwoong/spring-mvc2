18ae0463022d069380eaf97b3c269dc1d5507ff7
zwoong <zwoong6867@gmail.com>
2025-05-16 13:15:50 +0900

    ✨ 기본 객체들

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,10 +1,12 @@
 package hello.thymeleaf.basic;
 
+import jakarta.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,6 +46,19 @@ public class BasicController {
     model.addAttribute("userMap", map);
 
     return "basic/variable";
+  }
+
+  @GetMapping("/basic-objects")
+  public String basicObjects(HttpSession session) {
+    session.setAttribute("sessionData", "Hello Session");
+    return "basic/basic-objects";
+  }
+
+  @Component("helloBean")
+  static class HelloBean {
+    public String hello(String data) {
+      return "Hello " + data;
+    }
   }
 
   @Data
