24c57e30ae0c1055a86108b15d6e3236f30fda7e
zwoong <zwoong6867@gmail.com>
2025-05-16 13:23:23 +0900

    ✨ 유틸리티 객체와 날짜

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,6 +1,7 @@
 package hello.thymeleaf.basic;
 
 import jakarta.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -52,6 +53,12 @@ public class BasicController {
   public String basicObjects(HttpSession session) {
     session.setAttribute("sessionData", "Hello Session");
     return "basic/basic-objects";
+  }
+
+  @GetMapping("/date")
+  public String date(Model model) {
+    model.addAttribute("localDateTime", LocalDateTime.now());
+    return "basic/date";
   }
 
   @Component("helloBean")
