58f3a33617bd28abcaf93915fd02223058c7430c
zwoong <zwoong6867@gmail.com>
2025-05-21 10:24:55 +0900

    ✨ Bean Validation - 오브젝트 오류

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -5,8 +5,10 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 @Data
+@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantiry >= 10000", message = "총합이 10,000원 넘게 입력해주세요.")
 public class Item {
 
     private Long id;
